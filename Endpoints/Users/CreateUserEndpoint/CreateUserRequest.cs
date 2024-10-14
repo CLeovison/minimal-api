08182f2b27d619182ef00d9ca6bb3911958f327c
Leovison <cleovison@gmail.com>
2024-10-14 19:27:49 +0800

    fix: Changes on CreateUser

--- Endpoints/Users/CreateUserEndpoint/CreateUserRequest.cs
+++ Endpoints/Users/CreateUserEndpoint/CreateUserRequest.cs
@@ -1,11 +1,11 @@
 namespace UserApi.Request;
 
-public record CreateUserRespose
+public record CreateUserRequest
 {
     public string Username { get; init; } = default!;
     public string Password { get; init; } = default!;
     public string Firstname { get; init; } = default!;
     public string Lastname { get; init; } = default!;
     public string Email { get; init; } = default!;
-
+    public string role {get; init;} = default!;
 }
