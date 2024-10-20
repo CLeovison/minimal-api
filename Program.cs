3618b2f03502fe15e9d5cfefd3de5c86a8eb76d8
Clark Leovison Rey <cleovison@gmail.com>
2024-10-21 01:30:10 +0800

    build: Remove some packages/dependencies

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString();
 
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Fucks!");
-
 app.Run();
